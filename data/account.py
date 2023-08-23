b053717b2543d17dfcefc1c4bf84c9c6c464a9e1
Rostyslav <rostyslav.stryzhak@cd.p2h.com>
2023-08-23 16:29:52 +0300

    fix: update Account model

--- data/account.py
+++ data/account.py
@@ -9,7 +9,7 @@ class Account:
     personal_number: str | int
     password: str = UserInfo.PASSWORD
     email: str = ""
-    phone_number: str = field(default=RandomManager().random_phone_number())
+    phone_number: str = field(default_factory=RandomManager().random_phone_number)
     confirmation_code: str = "0000"
     language: str = "en"
     birth_day: str = "1430-01-01"
