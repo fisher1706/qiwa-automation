1cf7dc8c9db53ed5d7ae3559d4e8e00f48228f3f
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-08-30 17:13:42 +0300

    Pass qiwa urls as separate dataclass

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import dataclasses
 import os
 from pathlib import Path
 from typing import Literal, Optional
@@ -7,32 +8,52 @@ from typing import Literal, Optional
 from pydantic import BaseSettings, HttpUrl, RedisDsn
 
 Envs = Literal["local", "demo", "stage"]
+QiwaDomain = Literal["qiwa.tech", "qiwa.info"]
+
+
+@dataclasses.dataclass
+class QiwaUrls:
+    domain: QiwaDomain
+
+    def __post_init__(self) -> None:
+        self.api: str = f"api.{self.domain}"
+        self.spa: str = f"spa.{self.domain}"
+        self.auth: str = f"auth.{self.domain}"
+        self.um: str = f"um.{self.domain}"
+        self.self_subscription: str = f"self-subscription.{self.domain}"
+        self.certificates_validation: str = f"certificates-validation.{self.domain}"
+        self.qiwa_change_occupation: str = f"api-change-occupation.{self.domain}"
+        self.qiwa_ott_service: str = f"internal-ott-service.{self.domain}"
+        self.employee_transfer: str = f"employee-transfer.{self.domain}"
+        self.laborer_sso_auth_api: str = f"laborer-sso-auth-api.{self.domain}"
+        self.laborer_sso_auth: str = f"laborer-sso-auth.{self.domain}"
+        self.delegationservice: str = f"delegationservice.{self.domain}"
+        self.contract_management: str = f"contract-management.{self.domain}"
+        self.internal_payment: str = f"internal-payment.{self.domain}"
 
 
 class Settings(BaseSettings):
     project_env: str
 
-    env_url: HttpUrl
-    api_url: HttpUrl
+    qiwa_domain: QiwaDomain
+    qiwa_urls: Optional[QiwaUrls] = None
+
     mock_mlsd_url: HttpUrl
-    mock_nic_url: HttpUrl
-    mock_unifonic_url: HttpUrl
-    internal_payment: HttpUrl
-    elastic_url: HttpUrl
-    laborer_sso_api_url: HttpUrl
-    laborer_sso_ui_url: HttpUrl
-    redis_url: RedisDsn
-    allure_url: HttpUrl
-    contract_management: HttpUrl
 
     # Stage URLs
-    employee_transfer: Optional[HttpUrl]
     um_ibm_test: Optional[HttpUrl]
     ibm_url: Optional[HttpUrl]
     payment_gateway_url: Optional[HttpUrl]
 
     # Qiwa URLs
     # TODO: make domain based
+    employee_transfer: Optional[HttpUrl]
+    env_url: HttpUrl
+    api_url: HttpUrl
+    internal_payment: HttpUrl
+    laborer_sso_api_url: HttpUrl
+    laborer_sso_ui_url: HttpUrl
+    contract_management: HttpUrl
     qiwa_spa_url: HttpUrl
     qiwa_um_url: HttpUrl
     qiwa_self_subscription_url: HttpUrl
@@ -78,8 +99,10 @@ class Settings(BaseSettings):
 
 
 settings = Settings.for_env(os.getenv("ENV"))
+settings.qiwa_urls = QiwaUrls(settings.qiwa_domain)
 
 if __name__ == "__main__":
     # To check the actual config values on start,
     # simply running `python config.py`
     print(repr(settings))
+    print(settings.qiwa_urls.spa)
