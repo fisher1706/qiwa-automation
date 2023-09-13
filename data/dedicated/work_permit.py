bb75a6815a401b6f01e6162694712b2857b026aa
@taras.lobach <taras.lobach@cd.p2h.com>
2023-09-13 12:15:12 +0300

    fix: delete unnecessary parameters

--- data/dedicated/work_permit.py
+++ data/dedicated/work_permit.py
@@ -11,18 +11,8 @@ lo_wp_user_2 = User(
     sequence_number="2000739",
 )
 
-lo_wp_iqama_1 = User(
-    personal_number="2369026089"
-)
-lo_wp_iqama_2 = User(
-    personal_number="2350756413"
-)
-lo_wp_iqama_3 = User(
-    personal_number="2426490187"
-)
-lo_wp_iqama_4 = User(
-    personal_number="2401631367"
-)
-lo_wp_iqama_5 = User(
-    personal_number="2310334475",
-)
+lo_wp_iqama_1 = User(personal_number="2369026089")
+lo_wp_iqama_2 = User(personal_number="2350756413")
+lo_wp_iqama_3 = User(personal_number="2426490187")
+lo_wp_iqama_4 = User(personal_number="2401631367")
+lo_wp_iqama_5 = User(personal_number="2310334475")
