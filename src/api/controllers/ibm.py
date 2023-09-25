5bb724910d65f688f240a56a669dcdd2e2cbbf69
Dmytro Popovskiy <dmytro.popovskyi@p2h.com>
2023-09-25 17:57:39 +0100

    Add token model. Adjust get_economic_activity_id, get_first_unrelated_occupation methods

--- src/api/controllers/ibm.py
+++ src/api/controllers/ibm.py
@@ -6,9 +6,9 @@ import pytest
 
 import config
 import src
-from data.constants import HEADERS
 from data.dedicated.change_occupation import User
 from data.dedicated.services import Service
+from src.api.constants.auth import HEADERS, CLIENT_ID, CLIENT_SECRET
 from src.api.constants.ibm import IBMServicesRequest, IBMServicesResponse
 from src.api.http_client import HTTPClient
 from src.api.models.ibm.getsaudicert import GetSaudiCertificateRsBody
@@ -28,6 +28,7 @@ from src.api.payloads.ibm.getestablishmentinformation import (
     GetEstablishmentInformationPayload,
     GetEstablishmentInformationRq,
 )
+from src.api.payloads.ibm.token import Token
 from utils.assertion import assert_status_code
 
 
@@ -37,9 +38,24 @@ class IBMApiController:
         self.url = config.settings.ibm_url
         self.route = "/takamol/staging"
 
+    def _get_token(self):
+        payload = Token(
+            grant_type="client_credentials",
+            scope="IntegrationScope",
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+        )
+        response = self.client.post(
+            url=self.url,
+            endpoint=self.route + "/takamol-oauth/oauth2/token",
+            data=payload.dict(),
+        )
+        response = response.json()
+        return f"{response['token_type']} {response['access_token']}"
+
     @allure.step
     def get_work_permit_requests_from_ibm(
-        self, body: src.api.models.ibm.payloads.GetWorkPermitRequestsRq
+            self, body: src.api.models.ibm.payloads.GetWorkPermitRequestsRq
     ) -> IBMWorkPermitRequestList:
         payload = {
             IBMServicesRequest.GET_WORK_PERMIT_REQUESTS.value: {
@@ -55,7 +71,7 @@ class IBMApiController:
 
     @allure.step
     def get_saudization_certificate_from_ibm(
-        self, body: src.api.models.ibm.payloads.GetSaudiCertificateRq
+            self, body: src.api.models.ibm.payloads.GetSaudiCertificateRq
     ) -> IBMResponseData[GetSaudiCertificateRsBody]:
         payload = {
             IBMServicesRequest.GET_SAUDI_CERTIFICATE.value: {
@@ -71,7 +87,7 @@ class IBMApiController:
 
     @allure.step
     def validate_establishment_saudization_in_ibm(
-        self, body: src.api.models.ibm.payloads.ValidEstSaudiCertificateRq
+            self, body: src.api.models.ibm.payloads.ValidEstSaudiCertificateRq
     ) -> IBMResponseData:
         payload = {
             IBMServicesRequest.VALIDATE_EST_SAUDI_CERTIFICATE.value: {
@@ -87,7 +103,7 @@ class IBMApiController:
 
     @allure.step
     def get_change_occupation_requests_from_ibm(
-        self, body: Body
+            self, body: Body
     ) -> IBMResponseData[src.api.models.ibm.searchchangeoccupation.Body]:
         payload = {
             IBMServicesRequest.SEARCH_CHANGE_OCCUPATION.value: {
@@ -142,8 +158,8 @@ class IBMApiController:
         response = self.client.post(
             url=self.url,
             endpoint=self.route + "/qiwalo/createnewappointment",
-            json=payload.dict(),
             headers=HEADERS,
+            json=payload.dict(),
         )
         response = response.json()
         try:
@@ -158,35 +174,32 @@ class IBMApiController:
             TransactionId="0",
             ChannelId="Qiwa",
             SessionId="0",
-            RequestTime="2023-08-03 09:00:00.555",
-            ServiceCode="CNA00001",
+            RequestTime="2019-10-10 00:00:00.555",
+            ServiceCode="GEI00001",
             DebugFlag="1",
-            UserInfo=UserInfo(UserId=user.personal_number, IDNumber=user.personal_number),
         )
-        body = {
-            "Body": EstablishmentInformation(
-                LaborOfficeId=user.labor_office_id,
-                EstablishmentSequenceNumber=user.sequence_number,
-            )
-        }
+        body = EstablishmentInformation(
+            LaborOfficeId=user.labor_office_id,
+            EstablishmentSequanceNumber=user.sequence_number,
+        )
         payload = GetEstablishmentInformationPayload(
             GetEstablishmentInformationRq=GetEstablishmentInformationRq(Header=header, Body=body)
         )
-        print(payload)
         response = self.client.post(
             url=self.url,
             endpoint=self.route + "/qiwa/esb/getestablishmentinformation",
-            json=payload.dict(),
             headers=HEADERS,
+            json=payload.dict(),
         )
-        print(response)
-        return response.json()["EconomicActivityId"]
+        return response.json()['GetEstablishmentInformationRs']['Body']['EstablishmentDetails']["EconomicActivityId"]
 
     @allure.step
     def get_first_unrelated_occupation(self, economic_activity_id: str) -> int:
+        headers = HEADERS
+        headers['Authorization'] = self._get_token()
         response = self.client.get(
             url=self.url,
             endpoint=self.route + f"/qiwa/v2/economic-activity/{economic_activity_id}/occupations",
-            headers=HEADERS,
+            headers=headers,
         )
-        return response.json()[0]["descriptionAr"]
+        return response.json()['occupationsList'][0]["descriptionAr"]
