f1b53f5d010036c32d578fe2cf9e6f1289b2d7b5
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-09-26 17:34:19 +0300

    Refactor table component

--- src/ui/components/raw/table.py
+++ src/ui/components/raw/table.py
@@ -1,23 +1,37 @@
-from selene import have
-from selene.core.entity import Element
+from selene import browser, have
+from selene.core.condition import Condition
+from selene.core.entity import Collection, Element
+
+from utils.selene import index_of_element_by
 
 
 class Table:
-    def __init__(self, web_element: Element):
-        self.web_element = web_element
+    def __init__(self, web_element: Element | str = "table"):
+        self.web_element = (
+            browser.element(web_element)
+            if isinstance(web_element, str)
+            else web_element
+        )
         self.header = self.web_element.element("thead")
         self.body = self.web_element.element("tbody")
         self.headers = self.header.all("th")
         self.rows = self.body.all("tr")
 
-    def row(self, number: int) -> "Row":
-        return Row(self.rows.element(number - 1))
+    def row(self, number: int) -> Element:
+        return self.rows.element(number - 1)
 
+    def cells(self, *, row: int | Condition[Element]) -> Collection:
+        _row = (
+            self.row(row)
+            if isinstance(row, int)
+            else self.rows.element_by(row)
+        )
+        return _row.all('td').from_(1)
 
-class Row:
-    def __init__(self, web_element: Element):
-        self.web_element = web_element
-        self.cells = self.web_element.all("td")
-
-    def cell(self, label: str) -> Element:
-        return self.cells.element_by(have.attribute("data-label", label))
+    def cell(self, *, row: int | Condition[Element], column: int | str) -> Element:
+        column_index = (
+            column - 1
+            if isinstance(column, int)
+            else self.headers.get(index_of_element_by(have.text(column)))
+        )
+        return self.cells(row=row)[column_index]
