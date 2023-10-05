6d43dee62f482e5451c15ea93e5827e0c30ac714
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-10-05 12:39:23 +0300

    Add temporary implementation of using deepdiff in test

--- tests/api/shareable/work_permits/test_cancel_sadad_number.py
+++ tests/api/shareable/work_permits/test_cancel_sadad_number.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from pprint import pprint
 
 import pytest
 
@@ -6,6 +7,7 @@ from src.api.assertions.model import validate_model
 from src.api.models.qiwa.raw.work_permit.cancel_sadad import SuccessfulCancelling
 from src.api.models.qiwa.work_permit import cancel_sadad_ibm_error
 from utils.assertion import assert_status_code, assert_that
+from deepdiff import DeepDiff, Delta
 
 pytestmark = [pytest.mark.stage]
 
@@ -18,8 +20,8 @@ def test_cancelling_pending_payment_request(api, pending_payment_sadad_number):
 
     json = response.json()
     validate_model(json, model=SuccessfulCancelling)
-    assert_that(json["message"])\
-        .has("message_en")(f"SADAD bill {pending_payment_sadad_number} has been canceled successfully")\
+    assert_that(json["message"]) \
+        .has("message_en")(f"SADAD bill {pending_payment_sadad_number} has been canceled successfully") \
         .has("message_ar")(f"تم إلغاء معاملة سداد رقم {pending_payment_sadad_number} بنجاح")
 
 
@@ -30,11 +32,32 @@ def test_cancelling_canceled_request(api, canceled_sadad_number):
     assert_status_code(response.status_code).equals_to(HTTPStatus.UNPROCESSABLE_ENTITY)
 
     json = response.json()
-    validate_model(json, model=cancel_sadad_ibm_error)
-    assert_that(json["data"]["attributes"])\
-        .has("code")("MOF00005")\
-        .has("english-msg")("Cannot be canceled, the payment entry transaction is canceled")\
-        .has("arabic-msg")("لا يمكن الإلغاء، معاملة سداد المدخلة ملغاة")
+    actual = cancel_sadad_ibm_error.parse_obj(json)
+    expected = {
+        "data": {
+            "id": "-1",
+            "type": "ibm-error",
+            "attributes": {
+                "id": -1,
+                "code": "MOF00005",
+                "status": "ERROR",
+                "english-msg": "Cannot be canceled, the payment entry transaction is canceled",
+                "arabic-msg": "لا يمكن الإلغاء، معاملة سداد المدخلة ملغاة",
+                "transaction-id": "1696423501419",
+                "service-code": "CWPB0001"
+            }
+        }
+    }
+    expected = cancel_sadad_ibm_error.parse_obj(expected)
+    actual = actual.dict(exclude_unset=True, by_alias=True, exclude={"data": {"attributes": {"transaction_id"}}})
+    expected = expected.dict(exclude_unset=True, by_alias=True, exclude={"data": {"attributes": {"transaction_id"}}})
+    difference = DeepDiff(expected, actual, ignore_order=True)
+    assert not difference, difference
+    # validate_model(json, model=cancel_sadad_ibm_error)
+    # assert_that(json["data"]["attributes"])\
+    #     .has("code")("MOF00005")\
+    #     .has("english-msg")("Cannot be canceled, the payment entry transaction is canceled")\
+    #     .has("arabic-msg")("لا يمكن الإلغاء، معاملة سداد المدخلة ملغاة")
 
 
 def test_cancelling_incorrect_sadad_number(api):
@@ -47,7 +70,7 @@ def test_cancelling_incorrect_sadad_number(api):
 
     json = response.json()
     validate_model(json, model=cancel_sadad_ibm_error)
-    assert_that(json["data"]["attributes"])\
-        .has("code")("MOF00008")\
-        .has("english-msg")("Cannot be canceled, payment entry is incorrect")\
+    assert_that(json["data"]["attributes"]) \
+        .has("code")("MOF00008") \
+        .has("english-msg")("Cannot be canceled, payment entry is incorrect") \
         .has("arabic-msg")("لا يمكن الإلغاء، معاملة سداد المدخلة غير صحيحة")
