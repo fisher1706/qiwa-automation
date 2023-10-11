648051a83f6f5437ec5313e656a8711b666203b0
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-10-11 11:47:36 +0300

    Update API tests for work permit employees

--- tests/api/shareable/work_permits/conftest.py
+++ tests/api/shareable/work_permits/conftest.py
@@ -1,3 +1,4 @@
+import random
 from http import HTTPStatus
 
 import pytest
@@ -6,6 +7,8 @@ from data.mock_mlsd.establishment import Establishment
 from src.api import models
 from src.api.app import QiwaApi
 from src.api.constants.work_permit import WorkPermitStatus
+from src.api.models.qiwa.raw.work_permit.employees import Employee
+from utils.assertion import assert_status_code
 
 
 @pytest.fixture(scope="package")
@@ -50,3 +53,11 @@ def canceled_sadad_number(api) -> str:
 
     requests_list = models.qiwa.work_permit.work_permit_requests_list.parse_obj(response.json())
     return requests_list.data[0].attributes.bill_number
+
+
+@pytest.fixture
+def employee(api) -> Employee:
+    response = api.wp_request_api.get_employees()
+    assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
+    response_json = models.qiwa.work_permit.employees_data.parse_obj(response.json())
+    return Employee.parse_obj(random.choice(response_json.data).attributes)
