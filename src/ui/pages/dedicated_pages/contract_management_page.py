68d12d1be268c9a8c6ab743141012aa5c18250d0
Dmytro Popovskiy <dmytro.popovskyi@p2h.com>
2023-10-12 17:17:32 +0100

    Add typing Add method for filling role field

--- src/ui/pages/dedicated_pages/contract_management_page.py
+++ src/ui/pages/dedicated_pages/contract_management_page.py
@@ -13,6 +13,9 @@ class ContractManagementPage:
     btn_next_step = s("[type='submit']")
 
     dropdown = ss(".tippy-content li")
+
+    # Establishment Details
+    field_role = s("[name='role.en']")
     field_work_location = s("#workLocation")
     dropdown_education_level = s("#education")
     field_major = s("#speciality")
@@ -57,72 +60,76 @@ class ContractManagementPage:
     # Summary
     terms_checkbox = s("#terms + span")
 
-    def wait_until_title_verification_code_appears(self, text: dict, locale: str):
+    def wait_until_title_verification_code_appears(self, text: dict, locale: str) -> ContractManagementPage:
         locator = self.title_verification_code
         locator.wait_until(be.visible)
         locator.should(have.exact_text(text[locale]))
         return self
 
-    def proceed_2fa(self, otp_code: str = "0000"):
+    def proceed_2fa(self, otp_code: str = "0000") -> ContractManagementPage:
         for number, element in zip(otp_code, self.field_verification_code):
             element.perform(command.js.set_value("")).type(number)
         return self
 
-    def click_btn_verify(self):
+    def click_btn_verify(self) -> ContractManagementPage:
         self.btn_verify.click()
         return self
 
-    def click_btn_next_step(self):
+    def click_btn_next_step(self) -> ContractManagementPage:
         self.btn_next_step.click()
         return self
 
     # Establishment Details
-    def fill_field_work_location(self, data: str):
-        self.field_work_location.perform(command.js.set_value("")).type(data)
+    def fill_field_role(self, data: str) -> ContractManagementPage:
+        self.field_role.perform(command.js.set_value("")).type(data)
+        return self
+
+    def fill_field_work_location(self, data: str) -> ContractManagementPage:
+        self.field_work_location.click().perform(command.js.set_value("")).type(data)
         self.dropdown.element_by(have.text(data)).click()
         return self
 
-    def select_dropdown_education_level(self, data: str):
+    def select_dropdown_education_level(self, data: str) -> ContractManagementPage:
         self.dropdown_education_level.click()
         self.dropdown.element_by(have.text(data)).click()
         return self
 
-    def fill_field_major(self, data: str):
+    def fill_field_major(self, data: str) -> ContractManagementPage:
         self.field_major.perform(command.js.set_value("")).type(data)
         self.dropdown.element_by(have.text(data)).click()
         return self
 
-    def fill_field_iban_number(self, data: str):
+    def fill_field_iban_number(self, data: str) -> ContractManagementPage:
         self.field_iban_number.perform(command.js.set_value("")).type(data)
         return self
 
-    def fill_field_mobile_number(self, data: str):
+    def fill_field_mobile_number(self, data: str) -> ContractManagementPage:
         self.field_mobile_number.perform(command.js.set_value("")).type(data)
         return self
 
-    def fill_field_email(self, data: str):
+    def fill_field_email(self, data: str) -> ContractManagementPage:
         self.field_email.perform(command.js.set_value("")).type(data)
         return self
 
     # Contract Details
-    def fill_field_occupation(self, data: str):
+    def fill_field_occupation(self, data: str) -> ContractManagementPage:
         self.field_occupation.should(be.visible).perform(command.js.set_value("")).type(data)
         self.dropdown_occupation.hover().click()
         return self
 
-    def fill_field_job_title_en(self, data: str):
+    def fill_field_job_title_en(self, data: str) -> ContractManagementPage:
         self.field_job_title_en.perform(command.js.set_value("")).type(data)
         return self
 
-    def fill_field_job_title_ar(self, data: str):
+    def fill_field_job_title_ar(self, data: str) -> ContractManagementPage:
         self.field_job_title_ar.perform(command.js.set_value("")).type(data)
         return self
 
-    def fill_field_employee_number(self, data: str):
+    def fill_field_employee_number(self, data: str) -> ContractManagementPage:
         self.field_employee_number.hover().perform(command.js.set_value("")).type(data)
         return self
 
-    def fill_field_contract_period(self):
+    def fill_field_contract_period(self) -> ContractManagementPage:
         # TODO: Adjust method for different types of users
         # self.field_contract_period.should(be.visible).all("option").element_by(
         #     have.text(data)
@@ -130,88 +137,89 @@ class ContractManagementPage:
         self.radiobtn_contract_period_type.click()
         return self
 
-    def fill_field_trial_period(self, data: str):
+    def fill_field_trial_period(self, data: str) -> ContractManagementPage:
         self.field_trial_period.perform(command.js.set_value("")).type(data)
         return self
 
-    def select_type_of_work(self, data: str):
+    def select_type_of_work(self, data: str) -> ContractManagementPage:
         self.dropdown_contract_type.should(be.visible).all("option").element_by(
             have.text(data)
         ).click()
         return self
 
-    def fill_field_period(self, data: str):
+    def fill_field_period(self, data: str) -> ContractManagementPage:
         self.field_period.perform(command.js.set_value("")).type(data)
         return self
 
-    def fill_field_notice_period(self, data: str):
+    def fill_field_notice_period(self, data: str) -> ContractManagementPage:
         self.field_notice_period.perform(command.js.set_value("")).type(data)
         return self
 
-    def select_working_hours_type(self, data: str):
+    def select_working_hours_type(self, data: str) -> ContractManagementPage:
         self.dropdown_work_hours_type.should(be.visible).all("option").element_by(
             have.text(data)
         ).click()
         return self
 
-    def fill_field_days_per_week(self, data: str):
+    def fill_field_days_per_week(self, data: str) -> ContractManagementPage:
         self.field_days_per_week.perform(command.js.set_value("")).type(data)
         return self
 
-    def fill_field_daily_hours(self, data: str):
+    def fill_field_daily_hours(self, data: str) -> ContractManagementPage:
         self.field_daily_hours.perform(command.js.set_value("")).type(data)
         return self
 
-    def fill_field_hours_per_week(self, data: str):
+    def fill_field_hours_per_week(self, data: str) -> ContractManagementPage:
         self.field_hours_per_week.perform(command.js.set_value("")).type(data)
         return self
 
-    def fill_field_annual_vacations_days(self, data: str):
+    def fill_field_annual_vacations_days(self, data: str) -> ContractManagementPage:
         self.field_annual_vacations_days.perform(command.js.set_value("")).type(data)
         return self
 
-    def fill_field_basic_salary(self, data: str):
+    def fill_field_basic_salary(self, data: str) -> ContractManagementPage:
         self.field_basic_salary.perform(command.js.set_value("")).type(data)
         return self
 
-    def select_dropdown_salary_amount_type(self, data: str):
+    def select_dropdown_salary_amount_type(self, data: str) -> ContractManagementPage:
         self.dropdown_salary_amount_type.should(be.visible).all("option").element_by(
             have.text(data)
         ).click()
         return self
 
-    def select_dropdown_salary_period(self, data: str):
+    def select_dropdown_salary_period(self, data: str) -> ContractManagementPage:
         self.dropdown_salary_period.should(be.visible).all("option").element_by(
             have.text(data)
         ).click()
         return self
 
-    def fill_field_housing_allowance(self, data: str):
+    def fill_field_housing_allowance(self, data: str) -> ContractManagementPage:
         self.field_housing_allowance.perform(command.js.set_value("")).type(data)
+        return self
 
-    def select_dropdown_housing_allowance_type(self, data: str):
+    def select_dropdown_housing_allowance_type(self, data: str) -> ContractManagementPage:
         self.dropdown_housing_allowance_type.should(be.visible).all("option").element_by(
             have.text(data)
         ).click()
         return self
 
-    def select_dropdown_housing_allowance_frequency(self, data: str):
+    def select_dropdown_housing_allowance_frequency(self, data: str) -> ContractManagementPage:
         self.dropdown_housing_allowance_frequency.should(be.visible).all("option").element_by(
             have.text(data)
         ).click()
         return self
 
-    def fill_field_transportation_allowance(self, data: str):
+    def fill_field_transportation_allowance(self, data: str) -> ContractManagementPage:
         self.field_transportation_allowance.perform(command.js.set_value("")).type(data)
         return self
 
-    def select_dropdown_transportation_allowance_type(self, data: str):
+    def select_dropdown_transportation_allowance_type(self, data: str) -> ContractManagementPage:
         self.dropdown_transportation_allowance_type.should(be.visible).all("option").element_by(
             have.text(data)
         ).click()
         return self
 
-    def select_dropdown_transportation_allowance_frequency(self, data: str):
+    def select_dropdown_transportation_allowance_frequency(self, data: str) -> ContractManagementPage:
         self.dropdown_transportation_allowance_frequency.should(be.visible).all(
             "option"
         ).element_by(have.text(data)).click()
@@ -219,5 +227,5 @@ class ContractManagementPage:
 
     # Summary
     def select_terms_checkbox(self) -> ContractManagementPage:
-        self.terms_checkbox.hover().click()
+        self.terms_checkbox.should(be.visible).click()
         return self
