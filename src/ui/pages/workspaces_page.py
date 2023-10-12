022efc17141320dad70605d2e6f31f2cddc099e0
Dmytro Popovskiy <dmytro.popovskyi@p2h.com>
2023-10-12 17:19:45 +0100

    Add typing for the Workspaces page

--- src/ui/pages/workspaces_page.py
+++ src/ui/pages/workspaces_page.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
 import allure
-from selene import be, have
+from selene import be, have, browser
 from selene.support.shared.jquery_style import s, ss
+from selenium.common import NoSuchElementException
 
 
 class WorkspacesPage:
@@ -20,54 +23,60 @@ class WorkspacesPage:
     lmi_admin_card = ss("[data-component='Tile'] p").element_by(have.text("LMI Admin"))
 
     @allure.step
-    def should_have_workspace_list_appear(self):
+    def should_have_workspace_list_appear(self) -> WorkspacesPage:
         self.account_cards[0].wait_until(be.visible)
         return self
 
     @allure.step
-    def wait_page_to_load(self):
-        self.label_choose_account.with_(timeout=30).should(be.visible)
+    def wait_page_to_load(self) -> WorkspacesPage:
+        for _ in range(3):
+            try:
+                self.label_choose_account.should(be.visible)
+            except NoSuchElementException:
+                browser.driver.refresh()
+                continue
+        return self
 
     @allure.step
-    def select_admin_account(self):
+    def select_admin_account(self) -> WorkspacesPage:
         self.admin_account_card.click()
         return self
 
     @allure.step
-    def select_individual_account(self):
+    def select_individual_account(self) -> WorkspacesPage:
         self.individual_account_card.click()
         return self
 
     @allure.step
-    def select_lo_agent(self):
+    def select_lo_agent(self) -> WorkspacesPage:
         self.lo_agent_card.click()
         return self
 
     @allure.step
-    def select_business_account(self):
+    def select_business_account(self) -> WorkspacesPage:
         self.business_account_card.click()
         return self
 
     @allure.step
-    def select_first_company_account(self):
+    def select_first_company_account(self) -> WorkspacesPage:
         self.select_business_account()
         self.business_account_list.first.click()
         return self
 
     @allure.step
-    def select_company_account_with_sequence_number(self, sequence_number: int | str):
+    def select_company_account_with_sequence_number(self, sequence_number: int | str) -> WorkspacesPage:
         self.select_business_account()
         self.business_account_list.element_by(have.text(str(sequence_number))).click()
         return self
 
     @allure.step
-    def select_company_account_by_name(self, name: str):
+    def select_company_account_by_name(self, name: str) -> WorkspacesPage:
         self.select_business_account()
         self.search.type(name)
         self.business_account_list.second.click()
         return self
 
     @allure.step
-    def select_lmi_admin(self):
+    def select_lmi_admin(self) -> WorkspacesPage:
         self.lmi_admin_card.click()
         return self
