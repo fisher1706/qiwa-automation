e97792face197aed7a742b69009849d76e4fa88d
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-10-13 10:24:09 +0300

    Improve assertion func

--- src/api/assertions/diff.py
+++ src/api/assertions/diff.py
@@ -1,14 +1,31 @@
+import json
+
 import allure
 from deepdiff import DeepDiff
 
 
 @allure.step
-def assert_not_difference(entity1: dict, entity2: dict) -> None:
-    difference = DeepDiff(entity1, entity2)
+def assert_not_difference(expected: dict, actual: dict) -> None:
+    class SetEncoder(json.JSONEncoder):
+        def default(self, obj):
+            return list(obj)
+
+    difference = DeepDiff(expected, actual)
+
+    allure.attach(
+        json.dumps(expected, indent=2, ensure_ascii=False, cls=SetEncoder),
+        "Expected",
+        allure.attachment_type.JSON,
+    )
+    allure.attach(
+        json.dumps(actual, indent=2, ensure_ascii=False, cls=SetEncoder),
+        "Actual",
+        allure.attachment_type.JSON,
+    )
     if difference:
         allure.attach(
-            difference.pretty(),
-            "DIFFERENCE",
-            allure.attachment_type.TEXT,
+            json.dumps(difference, indent=2, ensure_ascii=False, cls=SetEncoder),
+            "Difference",
+            allure.attachment_type.JSON,
         )
         raise AssertionError(difference)
