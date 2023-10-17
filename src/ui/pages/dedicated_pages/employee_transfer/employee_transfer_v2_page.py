2b452f9b5e3e77e84993c061bdf74d75f62a7c1d
Dmytro Popovskiy <dmytro.popovskyi@p2h.com>
2023-10-17 11:10:36 +0100

    Modify old methods according to a new design

--- src/ui/pages/dedicated_pages/employee_transfer/employee_transfer_v2_page.py
+++ src/ui/pages/dedicated_pages/employee_transfer/employee_transfer_v2_page.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+from selene import have, query
+from selene.core.entity import Element
 from selene.support.shared.jquery_style import s, ss
 
+from src.ui.components.raw.table import Table
 from src.ui.pages.dedicated_pages.employee_transfer.summary_page import SummaryPage
 from src.ui.pages.dedicated_pages.employee_transfer.transfer_between_my_establishments_page import (
     TransferBetweenMyEstablishmentsPage,
@@ -22,6 +25,18 @@ class EmployeeTransferV2Page(
 
     btn_proceed_to_contract_management = s("//button[.='Proceed to Contract Management']")
 
+    tables = ss('table')
+    sent_requests_table = Table(tables.first)
+    received_requests_table = Table(tables.second)
+
+    items = ss('[data-component="Table"] + div > div > p')
+    sent_requests = items.first
+    received_requests = items.second
+
+    @staticmethod
+    def _get_count_of_requests(web_element: Element) -> int:
+        return int(web_element.get(query.text).split()[2])
+
     def click_btn_transfer_employee(self) -> EmployeeTransferV2Page:
         self.btn_transfer_employee.click()
         return self
@@ -37,3 +52,13 @@ class EmployeeTransferV2Page(
     def click_btn_proceed_to_contract_management(self) -> EmployeeTransferV2Page:
         self.btn_proceed_to_contract_management.click()
         return self
+
+    def check_count_of_sent_request_rows(self):
+        requests = EmployeeTransferV2Page._get_count_of_requests(self.sent_requests)
+        rows = requests if requests < 10 else 10
+        self.sent_requests_table.rows.should(have.size(rows))
+
+    def check_count_of_received_request_rows(self):
+        requests = EmployeeTransferV2Page._get_count_of_requests(self.received_requests)
+        rows = requests if requests < 10 else 10
+        self.received_requests_table.rows.should(have.size(rows))
