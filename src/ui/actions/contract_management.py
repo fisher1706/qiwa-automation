c11a18f135119583cb8df891153fa5c55cde908c
Dmytro Popovskiy <dmytro.popovskyi@p2h.com>
2023-10-18 22:23:55 +0100

    Add verification accordingly transfer_type for contract creation steps

--- src/ui/actions/contract_management.py
+++ src/ui/actions/contract_management.py
@@ -1,20 +1,20 @@
 from __future__ import annotations
 
-from data.constants import UserType
 from data.dedicated.contract_details import (
     ContractDetails,
     EmployeeDetails,
     EstablishmentDetails,
 )
+from data.dedicated.employee_trasfer.constants import type_4
+from data.dedicated.models.transfer_type import TransferType
 from src.ui.pages.dedicated_pages.contract_management_page import ContractManagementPage
 
 
 class ContractManagementActions(ContractManagementPage):
-    def fill_establishment_details(self, user_type: str) -> ContractManagementActions:
+    def fill_establishment_details(self) -> ContractManagementActions:
         establishment_details = EstablishmentDetails()
-        if user_type == UserType.EXPAT:
-            self.fill_field_role(establishment_details.role)
-            self.fill_field_email(establishment_details.company_email)
+        self.fill_field_role(establishment_details.role)
+        self.fill_field_email(establishment_details.company_email)
         self.fill_field_work_location(establishment_details.work_location)
         return self
 
@@ -29,14 +29,11 @@ class ContractManagementActions(ContractManagementPage):
         self.fill_field_email(employee_details.email)
         return self
 
-    def fill_contract_details(self, user_type: str = None) -> ContractManagementActions:
+    def fill_contract_details(self, transfer_type: TransferType) -> ContractManagementActions:
         contract_details = ContractDetails()
 
-        if user_type == UserType.EXPAT:
+        if transfer_type.code == type_4.code:
             self.fill_field_occupation(contract_details.occupation)
-        elif user_type == UserType.USER:
-            self.fill_field_occupation(contract_details.occupation)
-            self.fill_field_iban_number(contract_details.iban_number)
         self.fill_field_job_title_en(contract_details.job_title_en)
         self.fill_field_job_title_ar(contract_details.job_title_ar)
         self.fill_field_employee_number(contract_details.employee_number)
