0e8477981757348b8c78d6394c2568b68f467669
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-10-24 11:27:25 +0300

    Move work permits models to a separate module

--- src/api/models/qiwa/data.py
+++ src/api/models/qiwa/data.py
@@ -1,15 +1,8 @@
-from typing import Any, List, Literal, Type, TypeVar, Union
+from typing import Any, List, Literal, Type, Union
 
-from pydantic import BaseModel
-
-from src.api.constants.work_permit import WorkPermitStatus, WorkPermitStatusArabic
 from src.api.models.qiwa import raw
 from src.api.models.qiwa.raw.data import Data
 from src.api.models.qiwa.raw.relationships import Relationships
-
-StatusLiteralT = TypeVar("StatusLiteralT", bound=WorkPermitStatus)
-StatusIdLiteralT = TypeVar("StatusIdLiteralT", bound=WorkPermitStatusArabic)
-
 
 group = Data[str, Literal["group"], raw.e_service.Group, Relationships]
 service_group = Data[int, Literal["service-group"], raw.e_service.ServiceGroup, Relationships]
@@ -18,7 +11,6 @@ e_service = Data[Union[str, int], Literal["e-service"], raw.e_service.EService, 
 e_service_super = Data[int, Literal["e-service-super"], raw.e_service.EServiceSuper, Relationships]
 tag = Data[str, Literal["tag"], raw.e_service.Tag, Type[None]]
 tag_super = Data[int, Literal["tag-super"], raw.e_service.TagSuper, Type[None]]
-wp_dept_info = Data[str, Literal["wp-debt-info"], raw.work_permit.wp_debts.WPDebtInfo, Type[None]]
 saudization_certificate = Data[
     str,
     Literal["saudization-certificate"],
@@ -33,27 +25,3 @@ encrypted_saudization_certificate = Data[
 ]
 error = Data[str, Literal["error"], raw.saudi_certificate.Error, Type[None]]
 not_found = Data[Literal["-1"], Literal["not_found"], List[Any], Type[None]]
-work_permit_request = Data[
-    str,
-    Literal["work-permit-request"],
-    raw.work_permit.transaction.WorkPermitRequest[WorkPermitStatusArabic, WorkPermitStatus],
-    Type[None],
-]
-
-
-def work_permit_request_with_status(status: StatusLiteralT, status_id: StatusIdLiteralT):
-    return Data[
-        str,
-        Literal["work-permit-request"],
-        raw.work_permit.transaction.WorkPermitRequest[status, status_id],
-        Type[None],
-    ]
-
-
-work_permit_employees = Data[
-    str, Literal["work_permit_employees"], raw.work_permit.employees.Employee, Type[None]
-]
-
-
-def ibm_error(attributes: Type[BaseModel]) -> Type[Data]:
-    return Data[Literal["-1"], Literal["ibm-error"], attributes, Type[None]]
--- src/api/models/qiwa/work_permits.py
+++ src/api/models/qiwa/work_permits.py
@@ -1,25 +1,29 @@
 from typing import Literal, Type
 
 from src.api.constants.work_permit import WorkPermitStatus, WorkPermitStatusLiterals
-from src.api.models.qiwa import data
+from src.api.models.qiwa.data_attr import work_permits
 from src.api.models.qiwa.raw import work_permit
 from src.api.models.qiwa.raw.root import Root
 from src.api.models.qiwa.raw.work_permit import validate_expat
 
-transactions_data = Root[list[data.work_permit_request], Type[None], work_permit.transaction.Meta]
-work_permit_debts = Root[list[data.wp_dept_info], Type[None], work_permit.wp_debts.Meta]
+transactions_data = Root[
+    list[work_permits.work_permit_request], Type[None], work_permit.transaction.Meta
+]
+work_permit_debts = Root[list[work_permits.wp_dept_info], Type[None], work_permit.wp_debts.Meta]
 cancel_sadad_ibm_error = Root[
-    data.ibm_error(work_permit.cancel_sadad.CancellingError), Type[None], Type[None]
+    work_permits.ibm_error(work_permit.cancel_sadad.CancellingError), Type[None], Type[None]
 ]
 expat_validation = validate_expat.ValidateExpat[validate_expat.ValidationResult, Type[None]]
 expat_validation_error = validate_expat.ValidateExpat[Literal[False], validate_expat.Error]
-employees_data = Root[list[data.work_permit_employees], Type[None], work_permit.transaction.Meta]
+employees_data = Root[
+    list[work_permits.work_permit_employees], Type[None], work_permit.transaction.Meta
+]
 
 
 def transactions_data_with_status(status: Type[WorkPermitStatus]):
     status, status_id = WorkPermitStatusLiterals[status.name].value
     return Root[
-        list[data.work_permit_request_with_status(status, status_id)],
+        list[work_permits.work_permit_request_with_status(status, status_id)],
         Type[None],
         work_permit.transaction.Meta,
     ]
