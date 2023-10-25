c2d0b41b9c46c01c211b7bca310561849ebeeb64
Dmytro Popovskiy <dmytro.popovskyi@p2h.com>
2023-10-25 13:25:57 +0100

    Adjust individual actions

--- src/ui/actions/individual_actions.py
+++ src/ui/actions/individual_actions.py
@@ -1,21 +1,20 @@
-from data.constants import EmployeeTransfer, Language
-from data.dedicated.enums import ServicesAndTools
+from __future__ import annotations
+
+from data.constants import EmployeeTransfer
 from src.ui.pages.individual_page import IndividualPage
 
 
 class IndividualActions(IndividualPage):
-    def approve_request(self):
+    def approve_request(self) -> IndividualActions:
         self.click_btn_accept_the_request()
         self.click_btn_modal_accept_the_request()
+        return self
 
-    def reject_request(self, reason: str = EmployeeTransfer.REJECT_REASON):
+    def reject_request(self, reason: str = EmployeeTransfer.REJECT_REASON) -> IndividualActions:
         self.click_btn_reject_the_request()
         self.select_rejection_reason(reason)
         self.click_btn_modal_reject_the_request()
+        return self
 
-    def proceed_steps_for_verifying_et_request(self):
-        self.change_locale(Language.EN)
-        self.select_service(ServicesAndTools.EMPLOYEE_TRANSFER.value)
-        self.proceed_2fa()
-        self.select_first_view_request()
-        self.click_agree_checkbox()
+    def proceed_steps_for_verifying_et_request(self) -> IndividualActions:
+        return self
