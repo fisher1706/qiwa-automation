a6afd6b1ee4c249b0cbac47417a4f5135dc31dfd
Dmytro Popovskiy <dmytro.popovskyi@p2h.com>
2023-10-25 13:31:30 +0100

    Add back to employee transfer button

--- src/ui/pages/dedicated_pages/employee_transfer/summary_page.py
+++ src/ui/pages/dedicated_pages/employee_transfer/summary_page.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from selene import have
-from selene.support.shared.jquery_style import ss
+from selene.support.shared.jquery_style import s, ss
 
 from data.dedicated.enums import EmployeeTransferSuccessMsg
 from src.ui.components.raw.table import Table
@@ -11,11 +11,17 @@ class SummaryPage:
     success_msg = ss('[role="banner"] + div p').first
     table = Table()
 
+    btn_back_to_employee_transfer = s("//button[.='Back to Employee Transfer']")
+
     def check_success_msg(self) -> SummaryPage:
         self.success_msg.should(have.text(EmployeeTransferSuccessMsg.SUMMARY.value))
         return self
 
     def check_request_status(self) -> SummaryPage:
-        # todo: [dp] Move to test data status request
+        # TODO(dp): Move to test data status request
         self.table.cell(row=2, column=1).should(have.text("Pending employee’s approval"))
+        return self
+
+    def click_btn_back_to_employee_transfer(self) -> SummaryPage:
+        self.btn_back_to_employee_transfer.click()
         return self
