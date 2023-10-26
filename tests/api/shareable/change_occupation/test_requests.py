51345dcf99091ba2fd3f219fb76ae62f6d2b2092
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-10-26 10:16:08 +0300

    Improve getting by id test to validate values

--- tests/api/shareable/change_occupation/test_requests.py
+++ tests/api/shareable/change_occupation/test_requests.py
@@ -8,15 +8,17 @@ from utils.assertion.asserts import assert_data
 
 
 def test_getting_by_request_id(api):
-    data = api.change_occupation.get_random_request()
+    request_data = api.change_occupation.get_random_request()
+    laborer_data = request_data.laborers[0]
 
-    response = api.change_occupation.get_request(data.request_id)
+    response = api.change_occupation.get_request(request_data.request_id)
     assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
 
     json = request_by_id_data.parse_obj(response.json())
     assert_that(json.data).is_length(1)
-    assert_data(expected=data, actual=json.data)
-    assert_data(expected=data.laborers[0], actual=json.data)
+    request_by_id = json.data[0].attributes
+    assert_data(expected=request_data.dict(exclude={"id"}), actual=request_by_id.dict())
+    assert_data(expected=laborer_data.dict(exclude={"request_number"}), actual=request_by_id.dict())
 
 
 @pytest.mark.parametrize("page", [-1, 0, 1, 3])
