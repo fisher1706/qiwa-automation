471265c8b729c3af53bec7dafe2716942f0ed314
Dmytro Popovskiy <dmytro.popovskyi@p2h.com>
2023-10-26 17:30:04 +0100

    Remove mobile verification popup

--- src/ui/components/dedicated/mobile_verification_popup.py
+++
@@ -1,9 +0,0 @@
-from selene.support.shared.jquery_style import s
-
-from src.ui.components.code_verification_box import CodeVerificationBox
-
-
-# pylint: disable=too-few-public-methods
-class MobileVerificationPopup:
-    popup = CodeVerificationBox()
-    popup.confirm_button = s("[form='otp-form']")
