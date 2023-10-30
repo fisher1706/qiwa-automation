1c1bf9965028c2b471b8d8e1d546cfa19d51b97c
Dmytro Popovskiy <dmytro.popovskyi@p2h.com>
2023-10-30 15:34:19 +0000

    Redesign tests cases for quota verification

--- tests/ui/dedicated/employee_transfer/test_employee_transfer_another_establishment.py
+++ tests/ui/dedicated/employee_transfer/test_employee_transfer_another_establishment.py
@@ -5,50 +5,13 @@ from data.constants import EmployeeTransfer, Language
 from data.dedicated.employee_trasfer.employee_transfer import (
     employer,
     laborer,
-    laborer_between_my_establishments,
-    laborer_between_my_establishments_existing_contract,
 )
 from data.dedicated.enums import ServicesAndTools
 from src.api.clients.employee_transfer import EmployeeTransferApi
-from src.ui.actions.contract_management import ContractManagementActions
 from src.ui.actions.employee_transfer import EmployeeTransferActions
 from src.ui.actions.individual_actions import IndividualActions
 from src.ui.qiwa import qiwa
-
-
-@allure.title('Verify user able to submit ET request from my own establishment')
-def test_user_able_to_submit_et_request_from_my_own_establishment():
-    EmployeeTransferApi().post_prepare_laborer_for_et_request(laborer_between_my_establishments.login_id)
-    EmployeeTransferActions().navigate_to_et_service(employer)
-
-    qiwa.employee_transfer_page.click_btn_transfer_employee() \
-        .select_own_establishment() \
-        .click_btn_next_step() \
-        .search_by_iqama_number(laborer_between_my_establishments.login_id) \
-        .select_first_employee(laborer_between_my_establishments.login_id) \
-        .click_btn_next_step() \
-        .click_link_create_contract() \
-        .click_btn_proceed_to_contract_management()
-
-    qiwa.code_verification.fill_in_code().click_confirm_button()
-
-    contract_management_actions = ContractManagementActions()
-    contract_management_actions.click_btn_next_step() \
-        .fill_establishment_details() \
-        .click_btn_next_step() \
-        .fill_employee_details() \
-        .click_btn_next_step() \
-        .fill_contract_details(laborer_between_my_establishments.transfer_type) \
-        .click_btn_next_step() \
-        .select_terms_checkbox() \
-        .click_btn_next_step()
-
-    qiwa.employee_transfer_page.click_btn_next_step() \
-        .select_terms_checkbox() \
-        .click_btn_submit_request()
-
-    qiwa.employee_transfer_page.check_success_msg() \
-        .check_request_status()
+from utils.assertion import assert_that
 
 
 @allure.title('Verify user able to submit ET request from another establishment')
@@ -90,19 +53,6 @@ def test_if_laborer_already_has_a_contract_do_not_show_redirection_to_cm_button_
         .check_existence_of_a_contract()
 
 
-@allure.title("If laborer already has a contract, don't show redirection to CM button from my own establishment")
-def test_if_laborer_already_has_a_contract_do_not_show_redirection_to_cm_button_from_my_own_establishment():
-    EmployeeTransferActions().navigate_to_et_service(employer)
-
-    qiwa.employee_transfer_page.click_btn_transfer_employee() \
-        .select_own_establishment() \
-        .click_btn_next_step() \
-        .search_by_iqama_number(laborer_between_my_establishments_existing_contract.login_id) \
-        .select_first_employee(laborer_between_my_establishments_existing_contract.login_id) \
-        .click_btn_next_step() \
-        .check_existence_of_a_contract()
-
-
 @allure.title('AS-320 Verify Laborer is able to approve the ET request')
 def test_laborer_able_to_approve_et_request():
     EmployeeTransferApi().post_prepare_laborer_for_et_request()
@@ -113,20 +63,24 @@ def test_laborer_able_to_approve_et_request():
 
     qiwa.employee_transfer_page.click_btn_back_to_employee_transfer()
 
+    qiwa.header.click_on_menu().click_on_logout()
+    qiwa.login_page.wait_login_page_to_load()
+    qiwa.header.change_local(Language.EN)
+
     employee_transfer_actions.navigate_to_individual(laborer.login_id)
 
-    qiwa.code_verification.fill_in_code()\
+    qiwa.code_verification.fill_in_code() \
         .click_confirm_button()
 
     # TODO(dp): Remove after fixing an issue with changing the language
     qiwa.header.change_local(Language.EN)
 
-    qiwa.individual_page.select_service(ServicesAndTools.EMPLOYEE_TRANSFER.value)\
+    qiwa.individual_page.select_service(ServicesAndTools.EMPLOYEE_TRANSFER.value) \
         .click_agree_checkbox()
 
     individual_actions = IndividualActions()
-    individual_actions.approve_request()\
-        .wait_until_popup_disappears()\
+    individual_actions.approve_request() \
+        .wait_until_popup_disappears() \
         .verify_expected_status(EmployeeTransfer.LABORER_TYPE_9_STATUS_APPROVE[Language.EN])
     qiwa.header.change_local(Language.AR)
     individual_actions.verify_expected_status(EmployeeTransfer.LABORER_TYPE_9_STATUS_APPROVE[Language.AR])
@@ -142,9 +96,13 @@ def test_laborer_able_to_reject_et_request():
 
     qiwa.employee_transfer_page.click_btn_back_to_employee_transfer()
 
+    qiwa.header.click_on_menu().click_on_logout()
+    qiwa.login_page.wait_login_page_to_load()
+    qiwa.header.change_local(Language.EN)
+
     employee_transfer_actions.navigate_to_individual(laborer.login_id)
 
-    qiwa.code_verification.fill_in_code()\
+    qiwa.code_verification.fill_in_code() \
         .click_confirm_button()
 
     qiwa.individual_page.select_service(ServicesAndTools.EMPLOYEE_TRANSFER.value) \
@@ -154,8 +112,88 @@ def test_laborer_able_to_reject_et_request():
     qiwa.header.change_local(Language.EN)
 
     individual_actions = IndividualActions()
-    individual_actions.reject_request()\
-        .wait_until_popup_disappears()\
+    individual_actions.reject_request() \
+        .wait_until_popup_disappears() \
         .verify_expected_status(EmployeeTransfer.LABORER_STATUS_REJECT[Language.EN])
     qiwa.header.change_local(Language.AR)
     individual_actions.verify_expected_status(EmployeeTransfer.LABORER_STATUS_REJECT[Language.AR])
+
+
+@allure.title('AS-328 Quota (Establishment Balance) Should be decreased after submitting ET request')
+def test_quota_should_be_decreased_after_submitting_et_request():
+    EmployeeTransferApi().post_prepare_laborer_for_et_request()
+    employee_transfer_actions = EmployeeTransferActions()
+    employee_transfer_actions.navigate_to_et_service(employer)
+
+    establishment_balance = qiwa.employee_transfer_page.get_recruitment_quota()
+
+    employee_transfer_actions.create_et_request_from_another_establishment(laborer)
+
+    qiwa.employee_transfer_page.click_btn_back_to_employee_transfer()
+
+    qiwa.header.click_on_menu().click_on_logout()
+    qiwa.login_page.wait_login_page_to_load()
+    qiwa.header.change_local(Language.EN)
+
+    employee_transfer_actions.navigate_to_individual(laborer.login_id)
+
+    qiwa.code_verification.fill_in_code() \
+        .click_confirm_button()
+
+    # TODO(dp): Remove after fixing an issue with changing the language
+    qiwa.header.change_local(Language.EN)
+
+    qiwa.individual_page.select_service(ServicesAndTools.EMPLOYEE_TRANSFER.value) \
+        .click_agree_checkbox()
+
+    individual_actions = IndividualActions()
+    individual_actions.approve_request() \
+        .wait_until_popup_disappears() \
+        .verify_expected_status(EmployeeTransfer.LABORER_TYPE_9_STATUS_APPROVE[Language.EN])
+
+    qiwa.header.click_on_menu_individuals(laborer.login_id).click_on_logout()
+    qiwa.login_page.wait_login_page_to_load()
+
+    employee_transfer_actions.navigate_to_et_service(employer)
+
+    assert_that(establishment_balance - 1).equals_to(qiwa.employee_transfer_page.get_recruitment_quota())
+
+
+@allure.title('Quota (Establishment Balance) increased after rejection of ET request by Laborer')
+def test_quota_should_be_increased_after_rejection_of_et_request_by_laborer():
+    EmployeeTransferApi().post_prepare_laborer_for_et_request()
+    employee_transfer_actions = EmployeeTransferActions()
+    employee_transfer_actions.navigate_to_et_service(employer)
+
+    establishment_balance = qiwa.employee_transfer_page.get_recruitment_quota()
+
+    employee_transfer_actions.create_et_request_from_another_establishment(laborer)
+
+    qiwa.employee_transfer_page.click_btn_back_to_employee_transfer()
+
+    qiwa.header.click_on_menu().click_on_logout()
+    qiwa.login_page.wait_login_page_to_load()
+    qiwa.header.change_local(Language.EN)
+
+    employee_transfer_actions.navigate_to_individual(laborer.login_id)
+
+    qiwa.code_verification.fill_in_code() \
+        .click_confirm_button()
+
+    qiwa.individual_page.select_service(ServicesAndTools.EMPLOYEE_TRANSFER.value) \
+        .click_agree_checkbox()
+
+    # TODO(dp): Remove after fixing an issue with changing the language
+    qiwa.header.change_local(Language.EN)
+
+    individual_actions = IndividualActions()
+    individual_actions.reject_request() \
+        .wait_until_popup_disappears() \
+        .verify_expected_status(EmployeeTransfer.LABORER_STATUS_REJECT[Language.EN])
+
+    qiwa.header.click_on_menu_individuals(laborer.login_id).click_on_logout()
+    qiwa.login_page.wait_login_page_to_load()
+
+    employee_transfer_actions.navigate_to_et_service(employer)
+
+    assert_that(establishment_balance).equals_to(qiwa.employee_transfer_page.get_recruitment_quota())
