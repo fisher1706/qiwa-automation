27e33be7fed08422caf3887f29286a37e2c718fd
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-11-01 10:14:48 +0200

    Remove custom encoder from assert_data

--- utils/assertion/asserts.py
+++ utils/assertion/asserts.py
@@ -1,5 +1,4 @@
 import json
-from datetime import date, datetime
 from typing import Any, TypeVar
 
 import allure
@@ -22,27 +21,21 @@ def assert_status_code(code: int) -> AssertionMixin:
 
 @allure.step
 def assert_data(*, expected: Any, actual: Any) -> None:
-    class SetEncoder(json.JSONEncoder):
-        def default(self, obj):
-            if isinstance(obj, (datetime, date)):
-                return str(obj)
-            return list(obj)
-
     difference = DeepDiff(expected, actual)
 
     allure.attach(
-        json.dumps(expected, indent=2, ensure_ascii=False, cls=SetEncoder),
+        json.dumps(expected, indent=2, ensure_ascii=False, default=str),
         "Expected",
         allure.attachment_type.JSON,
     )
     allure.attach(
-        json.dumps(actual, indent=2, ensure_ascii=False, cls=SetEncoder),
+        json.dumps(actual, indent=2, ensure_ascii=False, default=str),
         "Actual",
         allure.attachment_type.JSON,
     )
     if "values_changed" in difference.keys():
         allure.attach(
-            json.dumps(difference["values_changed"], indent=2, ensure_ascii=False, cls=SetEncoder),
+            json.dumps(difference["values_changed"], indent=2, ensure_ascii=False, default=str),
             "Difference",
             allure.attachment_type.JSON,
         )
