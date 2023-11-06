64ebf51214248b18f4bf3e0a6f77dac947b99838
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-11-06 21:37:00 +0200

    Improve description and in_ method

--- utils/assertion/assertion_base.py
+++ utils/assertion/assertion_base.py
@@ -14,18 +14,24 @@ class AssertionBase:
         self.actual = actual
         self._description: str = ""
 
+    @property
+    def description(self) -> str:
+        if not self._description and isinstance(self.actual, (str, int)):
+            self._description = self.actual
+        return self._description
+
     def assert_(
         self, actual: T, expected: T, assertion: AssertionTypes, allure_title: str
     ) -> AssertionBase:
         operator, context = assertion.value
         error_message = self._error_template(actual, expected, context)
-        with allure.step(f"Assert that {self._description} {allure_title} {expected}"):
+        with allure.step(f"Assert that {self.description} {allure_title} {expected}"):
             assert operator(actual, expected), error_message
         return self
 
     def _error_template(self, actual: T, expected: T, context: str) -> str:
         return f"""
-            Checking: {self._description}
+            Checking: {self.description}
             Expected: {expected} {type(expected)}
             Actual: {actual} {type(actual)}
     
