4b77611aef24360dd59cd7f0236fcfdaad736b6c
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-11-09 12:52:18 +0200

    Adjust test flow

--- tests/api/shareable/change_occupation/test_requests_laborers.py
+++ tests/api/shareable/change_occupation/test_requests_laborers.py
@@ -10,36 +10,12 @@ from utils.assertion.asserts import assert_data, assert_that
 from utils.json_search import get_data_attribute, search_data_by_attributes
 
 
-def test_getting_empty_page(change_occupation):
-    requests_laborers = change_occupation.get_requests_laborers(per=10)
-    page = requests_laborers.meta.pages_count + 1
-
-    response = change_occupation.api.get_requests_laborers(page=page, per=10)
-    assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
-    assert_data(expected=empty_data(), actual=response.json())
-
-    response = change_occupation.api.get_requests_laborers(page=10000, per=10)
-    assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
-    assert_data(expected=empty_data(), actual=response.json())
-
-
 def test_getting_by_laborer_name(change_occupation):
     laborer = change_occupation.get_random_laborer()
 
     json = change_occupation.get_requests_laborers(laborer_name=laborer.laborer_name, per=1000)
     assert_that(json.data).is_not_empty().size_is(json.meta.total_entities)
     assert_that(search_data_by_attributes(json, laborer_name=laborer.laborer_name)).size_is(json.meta.total_entities)
-
-
-@pytest.mark.parametrize(
-    "query_parameter",
-    [{"laborer_id": 0000000000}, {"laborer_name": "Non-exist name"}],
-    ids=["laborer_name", "laborer_id"]
-)
-def test_getting_by_non_existent_value(change_occupation, query_parameter):
-    response = change_occupation.api.get_requests_laborers(page=1, per=10, **query_parameter)
-    assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
-    assert_data(expected=empty_data(), actual=response.json())
 
 
 def test_getting_by_laborer_id(change_occupation):
@@ -50,18 +26,6 @@ def test_getting_by_laborer_id(change_occupation):
 
     laborer_ids_from_json = get_data_attribute(json, "laborer_id_no")
     assert_that(set(laborer_ids_from_json)).size_is(1).contains(laborer.laborer_id_no)
-
-
-@pytest.mark.parametrize("status", list(RequestStatus))
-def test_getting_by_status_id_according_to_count(change_occupation, status):
-    count = change_occupation.get_requests_count_by_status(status).requests_count
-
-    response = change_occupation.api.get_requests_laborers(page=1, per=100, request_status=status.value)
-    assert_that(response).has(status_code=HTTPStatus.OK)
-
-    json = Root.parse_obj(response.json())
-    assert_that(json.data).size_is(count)
-    assert_that(json.meta).has(total_entities=count)
 
 
 @pytest.mark.parametrize("status", [
@@ -77,9 +41,38 @@ def test_getting_by_status_id(change_occupation, status):
     assert_that(set(status_ids_from_json)).size_is(1).contains(status.value)
 
 
-def test_getting_by_non_existent_status_id(change_occupation):
-    status = 1000
-    response = change_occupation.api.get_requests_laborers(page=1, per=100, request_status=status)
+@pytest.mark.parametrize("status", list(RequestStatus))
+def test_getting_by_status_id_according_to_count(change_occupation, status):
+    count = change_occupation.get_requests_count_by_status(status).requests_count
+
+    response = change_occupation.api.get_requests_laborers(page=1, per=100, request_status=status.value)
+    assert_that(response).has(status_code=HTTPStatus.OK)
+
+    json = Root.parse_obj(response.json())
+    assert_that(json.data).size_is(count)
+    assert_that(json.meta).has(total_entities=count)
+
+
+def test_getting_empty_page(change_occupation):
+    requests_laborers = change_occupation.get_requests_laborers(per=10)
+    page = requests_laborers.meta.pages_count + 1
+
+    response = change_occupation.api.get_requests_laborers(page=page, per=10)
+    assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
+    assert_data(expected=empty_data(), actual=response.json())
+
+    response = change_occupation.api.get_requests_laborers(page=10000, per=10)
+    assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
+    assert_data(expected=empty_data(), actual=response.json())
+
+
+@pytest.mark.parametrize(
+    "parameter",
+    [{"laborer_id": 2000000000}, {"laborer_name": "Non-exist name"}, {"request_status": 10000}],
+    ids=["laborer_id", "laborer_name", "request_status"]
+)
+def test_getting_by_non_existent_parameter_value(change_occupation, parameter):
+    response = change_occupation.api.get_requests_laborers(page=1, per=10, **parameter)
     assert_that(response).has(status_code=HTTPStatus.OK)
 
     json = Root.parse_obj(response.json())
@@ -87,4 +80,6 @@ def test_getting_by_non_existent_status_id(change_occupation):
     assert_that(json.meta).has(
         pages_count=0,
         total_entities=0,
+        total_count=0,
+        total_pages=0
     )
