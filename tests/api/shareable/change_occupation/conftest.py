80e3ad07fb8c8484b86a011a3f7f00166d424539
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-11-14 13:10:09 +0200

    Remove labor_office_id hardcode for request_number

--- tests/api/shareable/change_occupation/conftest.py
+++ tests/api/shareable/change_occupation/conftest.py
@@ -49,7 +49,7 @@ def laborer(change_occupation):  # TODO: select randomly
     laborer = Laborer()
     requests = change_occupation.get_requests_by_laborer(laborer.personal_number)
     for request in requests:
-        change_occupation.api.cancel_request("1" + request["request_number"])
+        change_occupation.api.cancel_request(request["request_number"])
     yield laborer
     requests = change_occupation.get_requests_by_laborer(laborer.personal_number)
     for request in requests:
@@ -61,7 +61,7 @@ def laborer2(change_occupation):  # TODO: select randomly
     laborer = Laborer(personal_number="2037659303")
     requests = change_occupation.get_requests_by_laborer(laborer.personal_number)
     for request in requests:
-        change_occupation.api.cancel_request("1" + request["request_number"])
+        change_occupation.api.cancel_request(request["request_number"])
     yield laborer
     requests = change_occupation.get_requests_by_laborer(laborer.personal_number)
     for request in requests:
