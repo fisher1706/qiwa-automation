e1022c77a33f7b55eea0e393cfe5b53220a73796
Dmytro Popovskiy <dmytro.popovskyi@p2h.com>
2023-11-14 21:14:15 +0000

    Add header model

---
+++ src/api/payloads/ibm/header.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from pydantic.main import BaseModel
+
+
+class UserInfo(BaseModel):
+    IDNumber: str
+
+
+class Header(BaseModel):
+    TransactionId: str
+    ChannelId: Optional[str]
+    SessionId: Optional[str]
+    RequestTime: str
+    MWRequestTime: Optional[str]
+    ServiceCode: str
+    DebugFlag: Optional[str]
+    UserInfo: Optional[UserInfo]
