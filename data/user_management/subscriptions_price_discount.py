1fc0f81466da1e92e3cb7008871a57c0f72679e8
Oleh Lazeba <oleh.lazeba@cd.p2h.com>
2023-11-21 20:49:22 +0200

    update subscription_price_discount [R0903] use @dataclasses

--- data/user_management/subscriptions_price_discount.py
+++ data/user_management/subscriptions_price_discount.py
@@ -1,12 +1,17 @@
-class SubscriptionDefaultPrice:  # pylint: disable=R0903
-    default_price_val_1100 = 1100
-    default_price_val_2300 = 2300
-    default_price_val_7000 = 7000
-    default_price_val_10000 = 10000
-    default_price_val_12000 = 12000
+import dataclasses
 
 
-class SubscriptionDiscount:  # pylint: disable=R0903
-    discount_val_100 = 1.00
-    discount_val_25 = 0.25
-    discount_val_10 = 0.10
+@dataclasses.dataclass
+class SubscriptionDefaultPrice:
+    default_price_val_1100: float = 1100
+    default_price_val_2300: float = 2300
+    default_price_val_7000: float = 7000
+    default_price_val_10000: float = 10000
+    default_price_val_12000: float = 12000
+
+
+@dataclasses.dataclass
+class SubscriptionDiscount:
+    discount_val_100: float = 1.00
+    discount_val_25: float = 0.25
+    discount_val_10: float = 0.10
