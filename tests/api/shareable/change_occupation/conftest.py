657e09f8b8500c5fd15e6df9e26f5d466580082e
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-11-28 12:37:17 +0200

    Remove parametrization of pagination tests

--- tests/api/shareable/change_occupation/conftest.py
+++ tests/api/shareable/change_occupation/conftest.py
@@ -1,12 +1,6 @@
 import pytest
 
-from src.api.clients.change_occupation import ChangeOccupationApi
 from src.api.controllers.change_occupation import ChangeOccupationController
-from src.api.models.qiwa.change_occupation import (
-    RequestsData,
-    RequestsLaborersData,
-    UsersData,
-)
 from src.api.payloads.raw.change_occupation import Laborer
 
 
@@ -16,30 +10,6 @@ def change_occupation() -> ChangeOccupationController:
         office_id="1", sequence_number="136401", personal_number="1048285405"
     )
     return controller
-
-
-def pytest_generate_tests(metafunc):
-    if "endpoint" in metafunc.fixturenames:
-        params = {
-            "/requests": (
-                ChangeOccupationApi.get_requests,
-                RequestsData,
-                ChangeOccupationController.get_requests,
-            ),
-            "/requests-laborers": (
-                ChangeOccupationApi.get_requests_laborers,
-                RequestsLaborersData,
-                ChangeOccupationController.get_requests_laborers,
-            ),
-            "/users": (
-                ChangeOccupationApi.get_users,
-                UsersData,
-                ChangeOccupationController.get_users,
-            ),
-        }
-        if metafunc.function.__name__ == "test_getting_empty_page":
-            params.pop("/requests-laborers")  # exclude /requests-laborers from test
-        metafunc.parametrize("endpoint", params.values(), ids=params.keys())
 
 
 @pytest.fixture
--- tests/api/shareable/change_occupation/test_requests.py
+++ tests/api/shareable/change_occupation/test_requests.py
@@ -1,6 +1,11 @@
+from http import HTTPStatus
+
 import pytest
 
-from utils.assertion import assert_that
+from data.shareable.expected_json.change_occupation.common import empty_data
+from src.api.models.qiwa.change_occupation import RequestsData
+from utils.assertion import assert_that, assert_status_code
+from utils.assertion.asserts import assert_data
 from utils.json_search import get_data_attribute
 
 # TODO: get by partial name
@@ -34,3 +39,74 @@ def test_getting_with_non_existent_value(change_occupation, value):
         pages_count=0,
         total_entities=0,
     )
+
+
+class TestPagination:
+    @pytest.mark.parametrize("page", [-1, 0, 1])
+    def test_getting_by_page(self, change_occupation, page):
+        response = change_occupation.api.get_requests(page=page, per=10)
+        assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
+
+        json = RequestsData.parse_obj(response.json())
+        assert_that(json.meta).has(
+            current_page=page,
+            pages_count=json.meta.total_pages,
+            total_entities=json.meta.total.value
+        )
+
+    def test_getting_last_page(self, change_occupation):
+        requests = change_occupation.get_requests(per=10)
+        last_page = requests.meta.pages_count
+
+        response = change_occupation.api.get_requests(page=last_page, per=10)
+        assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
+
+        json = RequestsData.parse_obj(response.json())
+        last_page_data_count = json.meta.total_entities - json.meta.from_
+        assert_that(json.data).size_is(last_page_data_count)
+        assert_that(json.meta).has(
+            current_page=last_page,
+            pages_count=last_page,
+            total_pages=last_page
+        )
+
+    def test_getting_empty_page(self, change_occupation):
+        requests = change_occupation.get_requests(per=10)
+        page = requests.meta.pages_count + 1
+
+        response = change_occupation.api.get_requests(page=page, per=10)
+        json = RequestsData.parse_obj(response.json())
+        assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
+        assert_data(expected=empty_data(), actual=json.dict())
+
+        response = change_occupation.api.get_requests(page=10000, per=10)
+        json = RequestsData.parse_obj(response.json())
+        assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
+        assert_data(expected=empty_data(), actual=json.dict())
+
+    @pytest.mark.parametrize("per_page", list(range(1, 11)))
+    def test_getting_per_page(self, change_occupation, per_page):
+        response = change_occupation.api.get_requests(page=1, per=per_page)
+        assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
+
+        json = RequestsData.parse_obj(response.json())
+        assert_that(json.data).size_is(per_page)
+        assert_that(json.meta).has(size=per_page)
+
+    def test_getting_max_items(self, change_occupation):
+        max_items = 100
+        data = change_occupation.get_requests()
+        total_entities = data.meta.total_entities
+
+        response = change_occupation.api.get_requests(page=1, per=total_entities)
+        assert_status_code(response.status_code).equals_to(HTTPStatus.OK)
+
+        json = RequestsData.parse_obj(response.json())
+        assert_that(json.data).size_is(max_items)
+        assert_that(json.meta).has(current_page=1)
+        assert_that(json.meta.pages_count).is_greater(1)
+        assert_that(json.meta.total_pages).equals_to(json.meta.pages_count)
+
+    def test_getting_zero_per_page(self, change_occupation):
+        response = change_occupation.api.get_requests(page=1, per=0)
+        assert_status_code(response.status_code).equals_to(HTTPStatus.UNPROCESSABLE_ENTITY)
