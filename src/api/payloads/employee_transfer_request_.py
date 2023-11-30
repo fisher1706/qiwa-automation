9506fb5bc4bc5e04f1c2fc7c381b2aa6ff6eaf34
Dmytro Popovskiy <dmytro.popovskyi@p2h.com>
2023-11-30 16:22:12 +0000

    Add employee_transfer_request_ae_payload

---
+++ src/api/payloads/employee_transfer_request_.py
@@ -0,0 +1,51 @@
+from data.dedicated.models.laborer import Laborer
+from data.dedicated.models.user import User
+from src.api.payloads.ibm.header import Header, UserInfo
+
+from src.api.payloads.ibm.submitcsrequests import SubmitCSRequestRqPayload, SubmitCSRequestRq, Body, DestinationDetails, \
+    LaborersDetailsList, LaborersDetailsItem, LaborerDetails, SourceDetails, LaborerNationality
+
+
+def employee_transfer_request_ae_payload(user: User, laborer: Laborer):
+    return SubmitCSRequestRqPayload(
+        SubmitCSRequestRq=SubmitCSRequestRq(
+            Header=Header(
+                TransactionId="0",
+                ChannelId="Qiwa",
+                SessionId="0",
+                RequestTime="2023-08-03 09:00:00.555",
+                ServiceCode="SCSR0003",
+                DebugFlag="1",
+                UserInfo=UserInfo(IDNumber=user.personal_number),
+            ),
+            Body=Body(
+                DestinationDetails=DestinationDetails(
+                    EstablishmentName="",
+                    LaborOfficeId=user.labor_office_id,
+                    SequenceNumber=user.sequence_number,
+                ),
+                LaborersDetailsList=LaborersDetailsList(
+                    LaborersDetailsItem=LaborersDetailsItem(
+                        LaborerDetails=LaborerDetails(
+                            LaborerName="",
+                            LaborerIdNo=laborer.login_id,
+                            LaborerNationality=LaborerNationality(
+                                Code="340",
+                                NameAr="",
+                                NameEn="",
+                            ),
+                            LaborerStatusCode="1",
+                            IqamaExpiryDate="2023-02-09",
+                            TransferTypeId=laborer.transfer_type.code,
+                        ),
+                        SourceDetails=SourceDetails(
+                            EstablishmentName="",
+                            EstablishmentId="",
+                            LaborOfficeId="1",
+                            SequenceNumber="224981",
+                        ),
+                    )
+                ),
+            ),
+        )
+    ).dict(exclude_none=True)
