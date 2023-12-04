5b16d7a1f0ebf5e04993b794ef5efa56dd583751
Yurii Zosin <yurii.zosin@cd.p2h.com>
2023-12-04 18:25:28 +0200

    added test run statistics to email body

--- utils/email_sender.py
+++ utils/email_sender.py
@@ -6,17 +6,39 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
-def send_email_from_outlook(sender, email_password, report, receivers, service_name):
+def send_email_from_outlook(sender, email_password, report, receivers, service_name, junitxml_report: str):
+
+    def email_template(total: int, passed: int, failed: int, skipped: int) -> str:
+        return (f"<p style='max-width: 90px';><font color='grey'> Total: {total}\n </p>"
+                f"<p style='max-width: 90px'><font color='green'> Passed: {passed}\n </p>"
+                f"<p style='max-width: 90px'><font color='red'> Failed: {failed}\n </p>"
+                f"<p style='max-width: 90px'><font color='orange'> Skipped: {skipped}\n </p>")
+
+    def parse_report(xml_report: str) -> tuple:
+        import xml.etree.ElementTree as ET
+
+        tree = ET.parse(xml_report)
+        root = tree.getroot()[0]
+        total = int(root.get("tests"))
+        failed = int(root.get("failures"))
+        skipped = int(root.get("skipped"))
+        passed = total - failed - skipped
+
+        return email_template(total, passed, failed, skipped)
+
+    statistics = parse_report(junitxml_report)
+
     password = email_password
     message = MIMEMultipart()
     message["From"] = sender
     message["To"] = ", ".join(receivers)
     message["Subject"] = "Automation daily report: " + service_name + " " + str(date.today())
 
-    html_content = """<html>
+    html_content = f"""<html>
              <head></head>
              <body>
-                 <p>Please find the daily report in attachment to this email.</p>
+                 <h3>Please find the daily report in attachment to this email.</h1>
+                 <h3>{statistics}</h2>
              </body>
              </html>"""
 
@@ -49,11 +71,10 @@ if __name__ == "__main__":
     parser.add_argument("email")
     parser.add_argument("password")
     parser.add_argument("attachment")
-    parser.add_argument("recipients", nargs="+")
+    parser.add_argument("recipients", nargs = "+")
     parser.add_argument("project_name")
+    parser.add_argument("report_template")
     args = parser.parse_args()
 
     print(args.recipients)
-    send_email_from_outlook(
-        args.email, args.password, args.attachment, args.recipients, args.project_name
-    )
+    send_email_from_outlook(args.email, args.password, args.attachment, args.recipients, args.project_name, args.report_template)
