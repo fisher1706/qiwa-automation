315b0e3c6bb7ff129083cf21fb734a4d52d75424
violetta.len <violetta.len@cd.p2h.com>
2023-12-07 20:00:40 +0200

    Fix UM UI tests

--- tests/ui/user_management/conftest.py
+++ tests/ui/user_management/conftest.py
@@ -1,5 +1,3 @@
-import pytest
-
 from data.dedicated.models.user import User
 from src.api.app import QiwaApi
 from src.api.models.qiwa.raw.user_management_models import SubscriptionCookie
@@ -12,14 +10,15 @@ from tests.conftest import prepare_data_for_free_subscription
 from utils.helpers import set_cookies_for_browser
 
 
-def log_in_and_open_user_management(user: User, language: str) -> QiwaApi:
+def log_in_and_open_user_management(user: User, language: str, has_access: bool = True) -> QiwaApi:
     qiwa_api = QiwaApi.login_as_user(user.personal_number).select_company(
         int(user.sequence_number)
     )
     cookies = qiwa_api.sso.oauth_api.get_context()
     qiwa.open_user_management_page()
     set_cookies_for_browser(cookies)
-    qiwa.main_page.wait_until_page_is_loaded()
+    if has_access:
+        qiwa.main_page.wait_until_page_is_loaded()
     qiwa.header.change_local(language)
     return qiwa_api
 
@@ -45,7 +44,7 @@ def get_subscription_cookie(owner: User) -> dict:
     ).dict(by_alias=True)
 
 
-def prepare_data_for_add_access_to_company(owner: User, qiwa_api: QiwaApi, users: list):
+def remove_establishment_from_subscription(owner: User, qiwa_api: QiwaApi, users: list):
     subscription_cookie = get_subscription_cookie(owner)
     for user in users:
         prepare_data_for_free_subscription(qiwa_api, subscription_cookie, user)
