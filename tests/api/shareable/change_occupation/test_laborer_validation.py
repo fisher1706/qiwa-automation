070b90c7f98c10b5105d581f1e6a59242c2fcbe3
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-12-12 12:24:40 +0200

    Remove unnecessary assertions

--- tests/api/shareable/change_occupation/test_laborer_validation.py
+++ tests/api/shareable/change_occupation/test_laborer_validation.py
@@ -31,10 +31,7 @@ def test_validation_laborer_not_eligible(change_occupation):
     response = change_occupation.api.validate_laborer(not_eligible_user.personal_number, occupation_code=216101)
     assert_status_code(response.status_code).equals_to(HTTPStatus.UNPROCESSABLE_ENTITY)
 
-    json = MultiLangErrorsData.parse_obj(response.json())
-    error = json.data[0]
-    assert_that(error.attributes.en_EN.details).is_not_empty()
-    assert_that(error.attributes.ar_SA.details).is_not_empty()
+    MultiLangErrorsData.parse_obj(response.json())
 
 
 def test_validation_for_laborer_with_request(change_occupation, laborer):
@@ -42,27 +39,18 @@ def test_validation_for_laborer_with_request(change_occupation, laborer):
     response = change_occupation.api.validate_laborer(laborer.personal_number, laborer.occupation_code)
     assert_status_code(response.status_code).equals_to(HTTPStatus.UNPROCESSABLE_ENTITY)
 
-    json = MultiLangErrorsData.parse_obj(response.json())
-    error = json.data[0]
-    assert_that(error.attributes.en_EN.details).is_not_empty()
-    assert_that(error.attributes.ar_SA.details).is_not_empty()
+    MultiLangErrorsData.parse_obj(response.json())
 
 
 def test_validation_with_non_existent_personal_number(change_occupation):
     response = change_occupation.api.validate_laborer(personal_number=12341512312, occupation_code=712501)
     assert_status_code(response.status_code).equals_to(HTTPStatus.UNPROCESSABLE_ENTITY)
 
-    json = MultiLangErrorsData.parse_obj(response.json())
-    error = json.data[0]
-    assert_that(error.attributes.en_EN.details).is_not_empty()
-    assert_that(error.attributes.ar_SA.details).is_not_empty()
+    MultiLangErrorsData.parse_obj(response.json())
 
 
 def test_validation_with_non_existent_occupation_code(change_occupation, laborer):
     response = change_occupation.api.validate_laborer(laborer.personal_number, occupation_code=11111)
     assert_status_code(response.status_code).equals_to(HTTPStatus.UNPROCESSABLE_ENTITY)
 
-    json = MultiLangErrorsData.parse_obj(response.json())
-    error = json.data[0]
-    assert_that(error.attributes.en_EN.details).is_not_empty()
-    assert_that(error.attributes.ar_SA.details).is_not_empty()
+    MultiLangErrorsData.parse_obj(response.json())
