d91cf07815dfe505c61edbc031a1372183f5ac1f
horilyikyrylo <kyrylo.horilyi@cd.p2h.com>
2023-12-12 12:11:17 +0200

    Add CorO tests on creating request

--- src/api/models/qiwa/raw/change_occupations/error.py
+++ src/api/models/qiwa/raw/change_occupations/error.py
@@ -1,9 +1,11 @@
+from pydantic import constr
+
 from src.api.models.qiwa.base import QiwaBaseModel
 
 
 class ErrorDetails(QiwaBaseModel):
     description: str | None = ...
-    details: str
+    details: constr(strict=True, min_length=1)
 
 
 class MultiLangErrorAttributes(QiwaBaseModel):
--- tests/api/shareable/correct_occupation/conftest.py
+++ tests/api/shareable/correct_occupation/conftest.py
@@ -1,10 +1,25 @@
 import pytest
 
 from src.api.app import QiwaApi
+from src.api.controllers.change_occupation import ChangeOccupationController
 from src.api.controllers.correct_occupation import CorrectOccupationController
+from src.api.models.qiwa.raw.correct_occupation.laborers import LaborerAttributes
 
 
 @pytest.fixture(scope="module")
 def correct_occupation() -> CorrectOccupationController:
     qiwa = QiwaApi.login_as_user("1048285405").select_company(sequence_number=136401)
     return CorrectOccupationController(qiwa.client)
+
+
+@pytest.fixture(scope="module")
+def change_occupation(correct_occupation) -> ChangeOccupationController:
+    return ChangeOccupationController(correct_occupation.api.http)
+
+
+@pytest.fixture
+def laborer(correct_occupation) -> LaborerAttributes:
+    laborer = correct_occupation.get_any_laborer()
+    correct_occupation.delete_request_in_ibm(laborer.laborer_id)
+    yield laborer
+    correct_occupation.delete_request_in_ibm(laborer.laborer_id)
